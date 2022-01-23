17d02529d397f69d37b2977931b4e6e1bdba4e2c
Joe Montenaro <jmontenaro@cloudera.com>
2022-01-23 23:38:07 +0000

    JSON pull working from /sample_data

--- application.py
+++ application.py
@@ -1,12 +1,37 @@
+import json
 import os
 from flask import Flask, render_template
 
-#app = Flask(__name__,static_url_path='')
 app = Flask(__name__)
 
 @app.route('/')
 def home():
   return render_template("home.html")
+
+@app.route('/sample_data')
+def sample_data():
+  bitcoin = {"name":"bitcoin", "pricing_history":[
+    {"date":"2020-01-01", "price":"100"},
+    {"date":"2020-02-01", "price":"105"},
+    {"date":"2020-03-01", "price":"90"}
+  ]}
+  ethereum = {"name":"ethereum", "pricing_history":[
+    {"date":"2020-01-01", "price":"100"},
+    {"date":"2020-02-01", "price":"105"},
+    {"date":"2020-03-01", "price":"90"}
+  ]}
+  tether = {"name":"tether", "pricing_history":[
+    {"date":"2020-01-01", "price":"100"},
+    {"date":"2020-02-01", "price":"105"},
+    {"date":"2020-03-01", "price":"90"}
+  ]}
+  joecoin = {"name":"joecoin", "pricing_history":[
+    {"date":"2020-01-01", "price":"100"},
+    {"date":"2020-02-01", "price":"105"},
+    {"date":"2020-03-01", "price":"90"}
+  ]}
+  
+  return json.dumps([bitcoin, ethereum, tether, joecoin])
 
 from IPython.display import HTML
 HTML("<a href='https://{}.{}'>Click ME!</a>".format(os.environ['CDSW_ENGINE_ID'],os.environ['CDSW_DOMAIN']))
