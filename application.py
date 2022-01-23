a17c8bb943d93640084d5b2da460211d45147eb7
Joe Montenaro <jmontenaro@cloudera.com>
2022-01-23 20:54:40 +0000

    Various updates

--- application.py
+++ application.py
@@ -1,11 +1,15 @@
 import os
 from flask import Flask, render_template
 
-app = Flask(__name__,static_url_path='')
+#app = Flask(__name__,static_url_path='')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
   return render_template("home.html")
 
+from IPython.display import HTML
+HTML("<a href='https://{}.{}'>Click ME!</a>".format(os.environ['CDSW_ENGINE_ID'],os.environ['CDSW_DOMAIN']))
+
 if __name__=="__main__":
-  app.run(host='127.0.0.1', port=int(os.environ['CDSW_APP_PORT']))
+  app.run(port=int(os.environ['CDSW_APP_PORT']))
