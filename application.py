9cfc06449195b5de5ef832b116065b9959a4454c
Joe Montenaro <jmontenaro@cloudera.com>
2022-01-23 19:47:01 +0000

    Updated to use render_templates

--- application.py
+++ application.py
@@ -1,11 +1,11 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__,static_url_path='')
 
 @app.route('/')
 def home():
-  return "<script> window.location.href = '/flask/home.html'</script>"
+  return render_template("home.html")
 
 if __name__=="__main__":
   app.run(host='127.0.0.1', port=int(os.environ['CDSW_APP_PORT']))
