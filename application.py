cf0e9b4e73de5c7e62487cbf39cd52a980731411
Joe Montenaro <jmontenaro@cloudera.com>
2022-01-24 01:55:39 +0000

    Working headers

--- application.py
+++ application.py
@@ -1,6 +1,10 @@
 import json
+import logging
 import os
 from flask import Flask, render_template
+
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
 
 app = Flask(__name__)
 
@@ -25,16 +29,11 @@ def sample_data():
     {"date":"2020-02-01", "price":"105"},
     {"date":"2020-03-01", "price":"90"}
   ]}
-  joecoin = {"name":"joecoin", "pricing_history":[
-    {"date":"2020-01-01", "price":"100"},
-    {"date":"2020-02-01", "price":"105"},
-    {"date":"2020-03-01", "price":"90"}
-  ]}
   
-  return json.dumps([bitcoin, ethereum, tether, joecoin])
+  return json.dumps([bitcoin, ethereum, tether])
 
 from IPython.display import HTML
-HTML("<a href='https://{}.{}'>Click ME!</a>".format(os.environ['CDSW_ENGINE_ID'],os.environ['CDSW_DOMAIN']))
+HTML("<a href='https://{}.{}'>Crypto Pricing App</a>".format(os.environ['CDSW_ENGINE_ID'],os.environ['CDSW_DOMAIN']))
 
 if __name__=="__main__":
   app.run(port=int(os.environ['CDSW_APP_PORT']))
